6936e9a8b508f13b7b8a139b61d680a166a6849a
Marvin Meneses <Marvin.Meneses@wdc.com>
2023-08-22 15:21:38 +0800

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,71 @@
-import Image from 'next/image'
+import ArticleTemplate from "@/components/ArticleTemplate";
+
+const blogs = [
+  {
+    id: 1,
+    title: "How to use the new React Context API",
+    description:
+      "Ut exercitation veniam voluptate enim reprehenderit qui consequat irure elit. Elit irure aliquip minim do. Ex quis magna culpa elit sunt. Aute non ea reprehenderit in voluptate velit fugiat elit ex. Labore enim nostrud occaecat exercitation aute nostrud duis elit dolore consectetur incididunt officia sunt elit.",
+    profileImage:
+      "https://lh3.googleusercontent.com/a/AAcHTtfhD6iRNNO0Us2ZFfS0u8m9APlEpEQ4IctxRLyB2PLi=s96-c",
+    created_by: "Marvin Meneses",
+    created_timestamp: "Aug 20",
+  },
+  {
+    id: 2,
+    title: "What is the purpose of Life?",
+    description:
+      "Ad tempor anim nostrud occaecat et incididunt occaecat nisi proident do. Nulla magna sit commodo nostrud cillum irure eu commodo cillum dolore cupidatat. Eiusmod qui quis magna amet id ipsum sit sint nostrud minim aliqua elit. Aliquip adipisicing fugiat non proident veniam minim quis exercitation sit dolor. Aliqua fugiat consequat consectetur nulla irure anim ullamco irure consectetur id eiusmod reprehenderit. Exercitation aliqua eu ea cillum veniam et nostrud tempor esse irure incididunt aliqua magna.",
+    profileImage:
+      "https://lh3.googleusercontent.com/a/AAcHTtfhD6iRNNO0Us2ZFfS0u8m9APlEpEQ4IctxRLyB2PLi=s96-c",
+    created_by: "Marvin Meneses",
+    created_timestamp: "Aug 20",
+  },
+  {
+    id: 3,
+    title: "How to use the new React Context API",
+    description:
+      "Ut exercitation veniam voluptate enim reprehenderit qui consequat irure elit. Elit irure aliquip minim do. Ex quis magna culpa elit sunt. Aute non ea reprehenderit in voluptate velit fugiat elit ex. Labore enim nostrud occaecat exercitation aute nostrud duis elit dolore consectetur incididunt officia sunt elit.",
+    profileImage:
+      "https://lh3.googleusercontent.com/a/AAcHTtfhD6iRNNO0Us2ZFfS0u8m9APlEpEQ4IctxRLyB2PLi=s96-c",
+    created_by: "Marvin Meneses",
+    created_timestamp: "Aug 20",
+  },
+  {
+    id: 4,
+    title: "How to use the new React Context API",
+    description:
+      "Ut exercitation veniam voluptate enim reprehenderit qui consequat irure elit. Elit irure aliquip minim do. Ex quis magna culpa elit sunt. Aute non ea reprehenderit in voluptate velit fugiat elit ex. Labore enim nostrud occaecat exercitation aute nostrud duis elit dolore consectetur incididunt officia sunt elit.",
+    profileImage:
+      "https://lh3.googleusercontent.com/a/AAcHTtfhD6iRNNO0Us2ZFfS0u8m9APlEpEQ4IctxRLyB2PLi=s96-c",
+    created_by: "Marvin Meneses",
+    created_timestamp: "Aug 20",
+  },
+  {
+    id: 5,
+    title: "How to use the new React Context API",
+    description:
+      "Ut exercitation veniam voluptate enim reprehenderit qui consequat irure elit. Elit irure aliquip minim do. Ex quis magna culpa elit sunt. Aute non ea reprehenderit in voluptate velit fugiat elit ex. Labore enim nostrud occaecat exercitation aute nostrud duis elit dolore consectetur incididunt officia sunt elit.",
+    profileImage:
+      "https://lh3.googleusercontent.com/a/AAcHTtfhD6iRNNO0Us2ZFfS0u8m9APlEpEQ4IctxRLyB2PLi=s96-c",
+    created_by: "Marvin Meneses",
+    created_timestamp: "Aug 20",
+  },
+];
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <main className="max-w-2xl mx-auto mt-20 flex flex-col gap-12">
+      {blogs.map((item) => (
+        <ArticleTemplate
+          key={item.id}
+          title={item.title}
+          description={item.description}
+          profileImage={item.profileImage}
+          created_by={item.created_by}
+          created_timestamp={item.created_timestamp}
         />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      ))}
     </main>
-  )
+  );
 }
