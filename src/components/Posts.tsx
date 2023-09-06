72f2329fa257aa658641b979617b818c436888c5
Marvin Meneses <Marvin.Meneses@wdc.com>
2023-09-06 10:51:41 +0800

    fixed document is not defined in production build

---
+++ src/components/Posts.tsx
@@ -0,0 +1,39 @@
+import Content from "@/components/Content";
+import { articleProps } from "@/model/seekAndShare.model";
+
+async function getData() {
+  const res = await fetch(`${process.env.NEXTAUTH_URL}/api/posts`, {
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
+export default async function Posts() {
+  const data = await getData();
+  return (
+    <>
+      <div className="-mt-14 bg-[#F1F1F1] px-4">
+        <main className="mx-auto flex max-w-3xl flex-col gap-12 pt-64 md:pt-80">
+          {data.map((item: articleProps) => (
+            <Content
+              key={item._id}
+              _id={item._id}
+              title={item.title}
+              content={item.content}
+              profileImage={item.profileImage}
+              created_by={item.created_by}
+              user_name={item.user_name}
+              createdAt={item?.createdAt}
+              borderBottom="bg-white"
+            />
+          ))}
+        </main>
+      </div>
+    </>
+  );
+}
