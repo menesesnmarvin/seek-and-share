a2f0ab6394875ac2a322df55b9db6e9b387ad09f
Marvin Meneses <Marvin.Meneses@wdc.com>
2023-09-07 09:29:25 +0800

    update landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <>
       <LandingPage />
-      <div className="-mt-14 bg-[#F1F1F1] px-4">
+      <div className=" -mt-20 bg-[#F1F1F1] px-4">
         <Posts />
       </div>
     </>
--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -4,10 +4,10 @@ const LandingPage = () => {
       <h1 className="pt-20 text-4xl font-bold md:text-8xl">
         Life is meant to be shared.
       </h1>
-      <div className="pt-6 text-lg font-medium md:text-2xl">
-        <h1 className="">A journey is always better when it is shared.</h1>
-        <h1>
-          Share your life experiences, testimony and your daily devotions.
+      <div className="pt-6 text-lg font-medium sm:mx-32 md:mx-40 md:text-2xl">
+        <h1 className="">
+          A journey is always better when it is shared. Share your life
+          experiences, testimony and your daily devotions.
         </h1>
         <div className="pt-8 md:pt-12">Start Writing and Reading...</div>
       </div>
